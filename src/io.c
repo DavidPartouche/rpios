a9011b1cf1371748c5ab645de54f3eccaa27d6f5
David Partouche <david.partouche@gmail.com>
2021-01-29 17:38:36 +0100

    Adding fifo and read from keyboard

--- src/io.c
+++ src/io.c
@@ -8,11 +8,15 @@ enum {
 
 enum {
     GPIO_MAX_PIN = 53,
+    GPIO_FUNCTION_OUT = 1,
     GPIO_FUNCTION_ALT5 = 2,
+    GPIO_FUNCTION_ALT3 = 7
 };
 
 enum {
     Pull_None = 0,
+    Pull_Down = 1,
+    Pull_up = 2
 };
 
 void mmio_write(long reg, unsigned int val) {
@@ -27,7 +31,7 @@ unsigned int gpio_call(unsigned int pin_number, unsigned int value, unsigned int
                        unsigned int field_max) {
     unsigned int field_mask = (1 << field_size) - 1;
 
-    if (pin_number > field_mask) return 0;
+    if (pin_number > field_max) return 0;
     if (value > field_mask) return 0;
 
     unsigned int num_fields = 32 / field_size;
@@ -58,9 +62,27 @@ unsigned int gpio_function(unsigned int pin_number, unsigned int value) {
     return gpio_call(pin_number, value, GPFSEL0, 3, GPIO_MAX_PIN);
 }
 
+void gpio_useAsAlt3(unsigned int pin_number) {
+    gpio_pull(pin_number, Pull_None);
+    gpio_function(pin_number, GPIO_FUNCTION_ALT3);
+}
+
 void gpio_useAsAlt5(unsigned int pin_number) {
     gpio_pull(pin_number, Pull_None);
     gpio_function(pin_number, GPIO_FUNCTION_ALT5);
+}
+
+void gpio_initOutputPinWithPullNone(unsigned int pin_number) {
+    gpio_pull(pin_number, Pull_None);
+    gpio_function(pin_number, GPIO_FUNCTION_OUT);
+}
+
+void gpio_setPinOutputBool(unsigned int pin_number, unsigned int onOrOff) {
+    if (onOrOff) {
+        gpio_set(pin_number, 1);
+    } else {
+        gpio_clear(pin_number, 1);
+    }
 }
 
 enum {
@@ -72,7 +94,10 @@ enum {
     AUX_MU_LCR_REG = AUX_BASE + 76,
     AUX_MU_MCR_REG = AUX_BASE + 80,
     AUX_MU_LSR_REG = AUX_BASE + 84,
+    AUX_MU_MSR_REG = AUX_BASE + 88,
+    AUX_MU_SCRATCH = AUX_BASE + 92,
     AUX_MU_CNTL_REG = AUX_BASE + 96,
+    AUX_MU_STAT_REG = AUX_BASE + 100,
     AUX_MU_BAUD_REG = AUX_BASE + 104,
     AUX_UART_CLOCK = 500000000,
     UART_MAX_QUEUE = 16 * 1024
@@ -80,22 +105,38 @@ enum {
 
 #define AUX_MU_BAUD(baud) ((AUX_UART_CLOCK/(baud*8))-1)
 
+unsigned char uart_output_queue[UART_MAX_QUEUE];
+unsigned int uart_output_queue_write = 0;
+unsigned int uart_output_queue_read = 0;
+
 void uart_init() {
     mmio_write(AUX_ENABLES, 1);
-    mmio_write(AUX_MU_IER_REG, 0);
-    mmio_write(AUX_MU_CNTL_REG, 0);
-    mmio_write(AUX_MU_LCR_REG, 3);
-    mmio_write(AUX_MU_MCR_REG, 0);
-    mmio_write(AUX_MU_IER_REG, 0);
-    mmio_write(AUX_MU_IIR_REG, 0xC6);
+//    mmio_write(AUX_MU_IER_REG, 0);
+//    mmio_write(AUX_MU_IIR_REG, 0xC6);
+//    mmio_write(AUX_MU_LCR_REG, 3);
+//    mmio_write(AUX_MU_MCR_REG, 0);
+//    mmio_write(AUX_MU_CNTL_REG, 0);
     mmio_write(AUX_MU_BAUD_REG, AUX_MU_BAUD(115200));
     gpio_useAsAlt5(14);
     gpio_useAsAlt5(15);
     mmio_write(AUX_MU_CNTL_REG, 3);
 }
 
+unsigned int uart_isOutputQueueEmpty() {
+    return uart_output_queue_read == uart_output_queue_write;
+}
+
+unsigned int uart_isReadByteReady() {
+    return mmio_read(AUX_MU_LSR_REG) & 0x01;
+}
+
 unsigned int uart_isWriteByteReady() {
     return mmio_read(AUX_MU_LSR_REG) & 0x20;
+}
+
+unsigned char uart_readByte() {
+    while (!uart_isReadByteReady());
+    return (unsigned char) mmio_read(AUX_MU_IO_REG);
 }
 
 void uart_writeByteBlockingActual(unsigned char c) {
@@ -103,9 +144,37 @@ void uart_writeByteBlockingActual(unsigned char c) {
     mmio_write(AUX_MU_IO_REG, (unsigned int) c);
 }
 
+void uart_loadOutputFifo() {
+    while (!uart_isOutputQueueEmpty() && uart_isWriteByteReady()) {
+        uart_writeByteBlockingActual(uart_output_queue[uart_output_queue_read]);
+        uart_output_queue_read = (uart_output_queue_read + 1) & (UART_MAX_QUEUE - 1);
+    }
+}
+
+void uart_writeByteBlocking(unsigned char c) {
+    unsigned int next = (uart_output_queue_write + 1) & (UART_MAX_QUEUE - 1);
+    while (next == uart_output_queue_read) uart_loadOutputFifo();
+
+    uart_output_queue[uart_output_queue_write] = c;
+    uart_output_queue_write = next;
+}
+
 void uart_writeText(char *buffer) {
     while (*buffer) {
-        if (*buffer == '\n') uart_writeByteBlockingActual('\r');
-        uart_writeByteBlockingActual(*buffer++);
+        if (*buffer == '\n') uart_writeByteBlocking('\r');
+        uart_writeByteBlocking(*buffer++);
+    }
+}
+
+void uart_drainOutputQueue() {
+    while (!uart_isOutputQueueEmpty()) uart_loadOutputFifo();
+}
+
+void uart_update() {
+    uart_loadOutputFifo();
+
+    if (uart_isReadByteReady()) {
+        unsigned char c = uart_readByte();
+        if (c == '\r') uart_writeText("\n"); else uart_writeByteBlocking(c);
     }
 }
--- src/kernel.c
+++ src/kernel.c
@@ -3,5 +3,5 @@
 void main() {
     uart_init();
     uart_writeText("Hello world!\n");
-    while (1);
+    while (1) uart_update();
 }
